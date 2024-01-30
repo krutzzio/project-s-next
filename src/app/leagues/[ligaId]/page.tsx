f4f0cd302fd6a4836d1122559d382ac945aacdeb
Krutzzio <gerard.costa.curr@gmail.com>
2024-01-30 15:49:36 +0100

    Final PlayerInfoPrev (others deleted)

--- src/app/leagues/[ligaId]/page.tsx
+++ src/app/leagues/[ligaId]/page.tsx
@@ -1,11 +1,8 @@
-import { PlayerInfoPrev } from "../../ui/PlayersInfo/PlayerInfoPrev";
-import { PlayerInfoPrev2 } from "../../ui/PlayersInfo/PlayerInfoPrev2";
-import { PlayerInfoPrev3 } from "../../ui/PlayersInfo/PlayerInfoPrev3";
-import { PlayerInfoPrev4 } from "../../ui/PlayersInfo/PlayerInfoPrev4";
+import { PlayerInfoPrev } from "@/app/ui/PlayerInfoPrev";
 import styles from "@/app/ui/grids-layout.module.css"
 import PLAYERS from "@/test/playersFetch.json"
 
-export default function Liga({params} : {params: any}) {
+export default function Liga({ params }: { params: any }) {
 
     const len = 16;
     const playersArray = Array(len).fill(0)
@@ -13,13 +10,9 @@ export default function Liga({params} : {params: any}) {
         <main className="mx-4">
             <h1 className="text-xl mb-4">{params.ligaId}</h1>
             <section className={styles.gridListPlayers}>
-                {/*
+                {
                     playersArray.map((player, i) => <PlayerInfoPrev key={i} infoPlayer={PLAYERS} />)
-    */}
-                <PlayerInfoPrev infoPlayer={PLAYERS} />
-                <PlayerInfoPrev2 infoPlayer={PLAYERS} />
-                <PlayerInfoPrev3 infoPlayer={PLAYERS} />
-                <PlayerInfoPrev4 infoPlayer={PLAYERS} />
+                }
             </section>
         </main>
     )
